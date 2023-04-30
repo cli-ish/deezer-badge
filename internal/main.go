29ce813bf6787e7b28575559a1a97bb58bc5c9c9
Vincent Schneider <vincent.schneider@sudile.com>
2023-04-30 02:44:57 -0700

    removed embed in main and fixed not needed variable name

--- internal/main.go
+++ internal/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	_ "embed"
 	"github.com/cli-ish/deezer-badge/internal/routes"
 )
 
--- internal/routes/index.go
+++ internal/routes/index.go
@@ -5,7 +5,7 @@ import (
 	"net/http"
 )
 
-func (bs *BadgeServer) getIndex(w http.ResponseWriter, r *http.Request) {
+func (bs *BadgeServer) getIndex(w http.ResponseWriter, _ *http.Request) {
 	tmpl := template.Must(template.ParseFS(badgeTemplate, "templates/index.gohtml", "templates/head.gohtml", "templates/footer.gohtml"))
 	w.Header().Set("Content-Type", "text/html")
 	err := tmpl.Execute(w, struct {
